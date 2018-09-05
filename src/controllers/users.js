1ddac4b14154236b47c6334b31f0503cff7783c8
Kartik Chauhan <lkchauhan.725@gmail.com>
2018-09-05 18:38:33 +0530

    mastt maza aarha hai
    Signed-off-by: Kartik Chauhan <lkchauhan.725@gmail.com>

---
+++ src/controllers/users.js
@@ -0,0 +1,11 @@
+const { User } = require('../models/db')
+
+async function createUser (username, password) {
+  try {
+    return await User.create({
+      username, password
+    })
+  } catch (e) {
+    throw (e)
+  }
+}
